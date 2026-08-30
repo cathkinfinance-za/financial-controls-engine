55f6c2ceb90e9a26056913566caacc29dbbf77bd
cathkinoperations-blip <cathkin.operations@gmail.com>
2026-08-30 17:36:15 +0200

    deleted file

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-import os
-import google.generativeai as genai
-
-genai.configure(api_key="AQ.Ab8RN6LpkQbJv7C7efi6G_Wpdo2b6xDB-yUUc-ZnIk3iBus41w")
-
-# Print available models to console
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
